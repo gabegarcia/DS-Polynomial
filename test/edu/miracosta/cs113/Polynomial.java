5f7fcd5ad01a4eb54446c5ceae1842bb0ee1b9dd
gabegarcia <gmgarcia1@gmail.com>
2021-02-14 20:01:38 -0800

    Create Polynomial.java file

---
+++ test/edu/miracosta/cs113/Polynomial.java
@@ -0,0 +1,15 @@
+package edu.miracosta.cs113;
+
+public class Polynomial {
+
+	public Term getNumTerms() {
+		Term temp = new Term();
+		return temp;
+	}
+	
+	public void addTerm(Term o) {
+		
+	}
+	
+	
+}
