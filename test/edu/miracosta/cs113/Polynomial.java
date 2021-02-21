c9ea0c02d919a4ffbd88ba088a3368ded211d64e
gabegarcia <gmgarcia1@gmail.com>
2021-02-20 21:20:08 -0800

    Polynomial.java updates

--- test/edu/miracosta/cs113/Polynomial.java
+++ test/edu/miracosta/cs113/Polynomial.java
@@ -10,6 +10,7 @@ public class Polynomial extends Term {
 	
 	//private Node<Term> tempNode = new Node<Term>();
 	private static int listSize = 0;
+
 	//private Node<Term> head = null;
 	
 	//Node inner class
@@ -32,54 +33,73 @@ public class Polynomial extends Term {
 	//addTerm is similar to addFirst()
 	public void addTerm(Term o) {
 		
-		System.out.print("myPoly.size(): " + myPoly.size() + "\n");
-		int i = 0;
+		System.out.print("Top of addTerm() myPoly.size(): " + myPoly.size() + "\n");
+		int count = 0;
 		LinkedList<Term> tempList = new LinkedList<Term>();
+		boolean sorted = false;
+		boolean newTermAdded = false;
+		
 		if(myPoly.size() == 0) {
 			myPoly.addFirst(o);
+		} else {
 			
-		} else if(myPoly.peekLast().getExponent() > o.getExponent()) {
-				System.out.print("peekLast(): " + myPoly.peekLast().getExponent() + "\n");
-				System.out.print("getExponent(): " + o.getExponent() + "\n");
-				myPoly.addLast(o);
-			} else {
-				//myPoly.addFirst(o); //TODO need to figure out how to insert if peekLast < getExponent()
-				
-				while(myPoly.size() != 0) {
-					
-					if(myPoly.peekLast().getExponent() < o.getExponent()) {
-						tempList.addLast(myPoly.removeLast());
-						if(myPoly.size() == 0) {
-							tempList.addFirst(o);
-						}
-					} else {
-						tempList.addFirst(o);
-						tempList.addFirst(myPoly.removeLast());
-						System.out.println("From tempList else statement: " + tempList);
-					}
-					
+			while(myPoly.size() != 0) {
+				System.out.println("myPoly.size(): " + myPoly.size());
+				if(o.getExponent() > myPoly.get(myPoly.size()-1).getExponent() /*&& myPoly.size() != 1*/) {
+					//System.out.println("o.getExponent():" + o.getExponent());
+					//System.out.println("myPoly.get(myPoly.size()-1).getExponent()" + myPoly.get(myPoly.size()-1).getExponent());
+					tempList.addFirst(myPoly.pollLast());
+					//System.out.println("tempList.addFirst(): " + tempList);
+					//System.out.println("myPoly:" + myPoly);
+				} else if(o.getExponent() == myPoly.get(myPoly.size()-1).getExponent()){
+					Term tempTerm = new Term(myPoly.get(myPoly.size()-1).getCoefficient() + o.getCoefficient(),o.getExponent());
+					tempList.addFirst(tempTerm);
+					myPoly.removeLast();
+					newTermAdded = true;
 				}
+					else{
+						//System.out.println("Added first" + o);
+					tempList.addFirst(o);
+					newTermAdded = true;
+					//System.out.println("tempList from else: " + tempList);
+					//System.out.println("myPoly.size(): " + myPoly.size());
+				}
+				if(myPoly.size() == 0 && !newTermAdded) {
+					tempList.addFirst(o);
+				}
+				 while(myPoly.size() != 0 && newTermAdded) {
+					 tempList.addFirst(myPoly.pollLast());
+			 }
 			}
-				
-				
+		} 
+		
+			
+		
+
+			System.out.print("myPoly.size() after while loop: " + myPoly.size() + "\n");
 				//then consider what to do when exponents are equal
 		
 				//repopulate myPoly
 				System.out.println("tempList.size(): " + tempList.size());
+				System.out.print("tempList before last while loop: " + tempList + "\n");
 				while(tempList.size() != 0) {
 					myPoly.addFirst(tempList.removeLast());
 				}
+				
+		
 				
 	System.out.print("From myPolynomial: " + myPoly + "\n");
 		
 		
 	}
 	
+	
+	
 		
 	public Term getTerm(int i) {
+		Term temp = myPoly.get(i);
 		
-		Term temp = new Term();
-		return temp; 
+		return temp;
 	}
 	
 	public void add(Polynomial p) {
@@ -99,7 +119,7 @@ public class Polynomial extends Term {
 		String polyStr, temp;
 		
 		//handle x = 0
-		if(listSize == 0)
+		if(myPoly.size() == 0)
 			return "0";
 		
 		temp = myPoly.toString();
